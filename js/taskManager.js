b6b8afd0fd3442865e83172e858a78ba89a52917
Jinok <joseo46@hotmail.com>
2021-06-16 17:21:29 +1000

    add category of status

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,10 +1,10 @@
 const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
   const html = `
-    <div class="col-xl-4 col-md-6 col-sm-12" data-task-id="${id}">
+    <div class="card-list" data-task-id="${id}">
     <div class="card shadow-sm p-2 mb-3">
       <div class="card-body">
         <!-- Task name --------------->
-        <div class="row mb-3 align-items-start justify-content-between">          
+        <div class="row mb-3 align-items-start justify-content-between">
           <h5 class="card-title col-8">${name}</h5>
           <span class="col-3 badge pb-1 text-capitalize me-3 ${
             status === 'todo'
@@ -16,8 +16,8 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
               : status === 'done'
               ? ' bg-secondary'
               : ''
-          }              
-          ">${status}</span>          
+          }
+          ">${status}</span>
         </div>
         <!-- Assigned to -------------->
         <div class="row mb-3">
@@ -32,9 +32,13 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
         <!-- Date, edit and delete buttons ---->
         <div class="row align-items-center">
           <div class="col text-primary">${dueDate}</div>
-          <div class="col d-flex justify-content-end">           
-           ${ status === 'done' ? '' : '<span class="d-flex align-items-center" id="done-container"><button type="button" class="btn btn-outline-secondary btn-sm done-button">Done</button></span>' } 
-            <span class="d-flex justify-content-center "><i class="bi bi-x-lg icon-btn delete-button"></i></span>            
+          <div class="col d-flex justify-content-end">
+           ${
+             status === 'done'
+               ? ''
+               : '<span class="d-flex align-items-center" id="done-container"><button type="button" class="btn btn-outline-secondary btn-sm done-button">Done</button></span>'
+           }
+            <span class="d-flex justify-content-center "><i class="bi bi-x-lg icon-btn delete-button"></i></span>
           </div>
         </div>
       </div>
@@ -59,11 +63,15 @@ class TaskManager {
       status,
     });
   }
+
   render() {
-    let tasksHtmlList = [];
-    for (let i = 0; i < this.tasks.length; i++) {
-      let task = this.tasks[i];
-      // console.log(task)
+    let doneHtmlList = [];
+    let reviewHtmlList = [];
+    let todoHtmlList = [];
+    let inprogressHtmlList = [];
+    // let todoHtmlList = [];
+
+    this.tasks.forEach(task => {
       let date = new Date(task.dueDate);
       let formattedDate = date.toLocaleDateString();
       let taskHtml = createTaskHtml(
@@ -74,11 +82,49 @@ class TaskManager {
         formattedDate,
         task.status
       );
-      tasksHtmlList.push(taskHtml);
-    }
-    let tasksHtml = tasksHtmlList.join('\n');
-    document.getElementById('taskList').innerHTML = tasksHtml;
+
+      if (task.status === 'review') {
+        reviewHtmlList.push(taskHtml);
+      } else if (task.status === 'todo') {
+        todoHtmlList.push(taskHtml);
+      } else if (task.status === 'in progress') {
+        inprogressHtmlList.push(taskHtml);
+      } else {
+        doneHtmlList.push(taskHtml);
+      }
+      // tasksHtmlList.push(taskHtml);
+      let reviewHtml = reviewHtmlList.join('\n');
+      document.getElementById('review').innerHTML = reviewHtml;
+      let todoHtml = todoHtmlList.join('\n');
+      document.getElementById('todo').innerHTML = todoHtml;
+      let inprogressHtml = inprogressHtmlList.join('\n');
+      document.getElementById('inprogress').innerHTML = inprogressHtml;
+      let doneHtml = doneHtmlList.join('\n');
+      document.getElementById('done').innerHTML = doneHtml;
+    });
+
+    // let tasksHtml = tasksHtmlList.join('\n');
+
+    // let tasksHtmlList = [];
+    // for (let i = 0; i < this.tasks.length; i++) {
+    //   let task = this.tasks[i];
+    //   // console.log(task)
+    //   let date = new Date(task.dueDate);
+    //   let formattedDate = date.toLocaleDateString();
+    //   let taskHtml = createTaskHtml(
+    //     task.id,
+    //     task.name,
+    //     task.description,
+    //     task.assignedTo,
+    //     formattedDate,
+    //     task.status
+    //   );
+    //   tasksHtmlList.push(taskHtml);
+    // }
+    // let tasksHtml = tasksHtmlList.join('\n');
+    // document.getElementById('taskList').innerHTML = tasksHtml;
   }
+
   save() {
     // Create a string for all tasks
     let tasksJson = JSON.stringify(this.tasks);
@@ -91,21 +137,17 @@ class TaskManager {
   }
   load() {
     // check if any tasks are saved in localStorage
-    if (localStorage.getItem('tasks') !== null){ 
+    if (localStorage.getItem('tasks') !== null) {
       const tasksJson = localStorage.getItem('tasks');
       // Convert the tasksJson string to an array and store it in this.tasks
       this.tasks = JSON.parse(tasksJson);
-
     }
-   
     // check if the currentId is saved in localStorage
     if (localStorage.getItem('currentId') !== null) {
-    const currentId = localStorage.getItem('currentId');
-    //Convert the currentId to a number before storing in this.currentId
-    this.currentId = parseInt(currentId);
-
+      const currentId = localStorage.getItem('currentId');
+      //Convert the currentId to a number before storing in this.currentId
+      this.currentId = parseInt(currentId);
     }
-   
   }
   getTaskById(taskId) {
     let foundTask;
@@ -117,19 +159,13 @@ class TaskManager {
     });
     return foundTask;
   }
-
-
   deleteTask(taskId) {
     let newTasks = [];
     this.tasks.forEach(task => {
       if (task.id !== taskId) {
-        newTasks.push(task)
+        newTasks.push(task);
       }
-    })
-
+    });
     this.tasks = newTasks;
   }
 }
-
-
-
